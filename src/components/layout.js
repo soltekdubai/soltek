6d6113fa3687e46718e207fe67633f3905a31d4c
skelleng <skelleng@pm.me>
2024-10-17 09:44:42 +0000

    purge

--- src/components/layout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Layout = ({ children }) => (
-  <div>
-    <header className="text-3xl text-center">SolTek</header>
-    <main>{children}</main>
-    <footer className="text-center">© 2024 SolTek, All Rights Reserved</footer>
-  </div>
-);
-export default Layout;
--- src/pages/about.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const Layout = () => (
-  <div>
-    <h1 className="text-4xl">About SolTek</h1>
-    <p>SolTek is dedicated to providing innovative and sustainable solar energy solutions across the UAE.</p>
-  </div>
-);
-export default Layout;
